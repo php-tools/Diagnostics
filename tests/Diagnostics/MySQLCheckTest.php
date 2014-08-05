87c2908f3682a21bac5abbdfebd6be3fdefc85ea
Luis Mayta <slovacus@gmail.com>
2014-08-05 15:29:46 -0500

    added provider data test

--- tests/Diagnostics/MySQLCheckTest.php
+++ tests/Diagnostics/MySQLCheckTest.php
@@ -3,6 +3,7 @@
 namespace Diagnostics\Tests;
 
 use PHPUnit_Framework_TestCase;
+use Diagnostics\Check\MySQLCheck;
 
 class MySQLCheckTest extends PHPUnit_Framework_TestCase
 {
@@ -10,6 +11,32 @@ class MySQLCheckTest extends PHPUnit_Framework_TestCase
     public function testConfig()
     {
 
+    }
+
+    public function configProvider()
+    {
+        return array(
+            array(
+                "hostname" => "127.0.0.1"
+            ),
+            array(
+                "hostname" => "127.0.0.1",
+                "username" => "root",
+                "password" => "root",
+                "database" => "db",
+            ),
+            array(
+                "hostname" => "127.0.0.1",
+                "username" => "root",
+                "password" => "root",
+                "database" => "db",
+                "port" => "3307"
+            ),
+        );
+    }
+
+    public function getMockAdapterMysql()
+    {
     }
 
 }
